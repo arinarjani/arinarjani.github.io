ce5881f9f46d068dc408f6c4db7fa39aecdf0382
arinarjani <arinarjani@gmail.com>
2016-04-29 23:09:46 -0700

    web files

---
+++ util.js
@@ -0,0 +1,12 @@
+/*
+original: http://www.chrismorata.com/resume/
+Author: Chris Morata
+*/
+
+
+$(document).ready(function(){
+	// print buttons
+	$(".print").bind("click", function(){
+		window.print();
+	});
+});
