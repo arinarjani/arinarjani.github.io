fe0d256d9a29bb34f3ed7fc4f7f2bae6e4b2a4eb
Arin <arinarjani@gmail.com>
2017-02-03 15:45:46 -0800

    Commented out smooth scrolling for now
    I want to use fullpage.js

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+// // A JQuery snippet for smooth scrolling
 // $(function() {
 //   $('a[href*="#"]:not([href="#"])').click(function() {
 //     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -13,20 +13,4 @@
 //     }
 //   });
 // });
-=======
-// A JQuery snippet for smooth scrolling
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
->>>>>>> 1b231b078b7e1141b5b9d98bca129b7273de7a11
+
